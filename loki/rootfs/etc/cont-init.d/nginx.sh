3dd663f3804f24c965b22f80da8dd034223500b7
Mike Degatano <michael.degatano@gmail.com>
2021-04-10 15:01:47 -0400

    Correctly use new cafile variable

--- loki/rootfs/etc/cont-init.d/nginx.sh
+++ loki/rootfs/etc/cont-init.d/nginx.sh
@@ -29,15 +29,13 @@ if bashio::config.true 'ssl'; then
             cafile="/ssl/${cafile}"
         fi
 
-        if ! bashio::fs.file_exists "$(bashio::config 'cafile')"; then
+        if ! bashio::fs.file_exists "${cafile}"; then
 	        bashio::log.fatal
 	        bashio::log.fatal "The file specified for 'cafile' does not exist!"
 	        bashio::log.fatal "Ensure the CA certificate file exists and full path is provided"
 	        bashio::log.fatal
 	        bashio::exit.nok
 	    fi
-
-        cafile=$(bashio::config 'cafile')
 	
 	    mv /etc/nginx/servers/direct-mtls.disabled /etc/nginx/servers/direct.conf
 	    sed -i "s#%%cafile%%#${cafile}#g" /etc/nginx/servers/direct.conf
